570b2d2892cadf8f58d469855bbc49f05d52a88c
JosephQuayle <josephquayle@hotmail.com>
2026-01-31 01:40:02 +0000

    more additions

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function showButton() {
   heroVideo.classList.add("video-dark");
   button.style.display = "block";
 }
+
+const subNav = document.getElementById("subnav");
+const observer = new IntersectionObserver(
+  ([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
+  { threshold: [1] },
+);
+observer.observe(subNav);
